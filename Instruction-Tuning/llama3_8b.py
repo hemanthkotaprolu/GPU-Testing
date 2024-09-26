cbc1346c2260cc853bb22667a2182aa37d5be5d2
Kotaprolu Hemanth <92506047+hemanthkotaprolu@users.noreply.github.com>
2024-09-26 19:55:30 +0530

    modify code

--- Instruction-Tuning/llama3_8b.py
+++ Instruction-Tuning/llama3_8b.py
@@ -79,25 +79,7 @@ def template(item, eval):
 def tokenize_function(examples):
     results =  tokenizer(examples['text'], padding="max_length", truncation=True, return_tensors='pt', max_length=512)
     results['labels'] = results['input_ids']
-    return result
-
-def compute_metrics(eval_preds):
-    preds, labels = eval_preds
-    # preds have the same shape as the labels, after the argmax(-1) has been calculated
-    # by preprocess_logits_for_metrics but we need to shift the labels
-    labels = labels[:, 1:].reshape(-1)
-    preds = preds[:, :-1].reshape(-1)
-    mask = labels != -100
-    labels = labels[mask]
-    preds = preds[mask]
-    return metric.compute(predictions=preds, references=labels)
-
-def preprocess_logits_for_metrics(logits, labels):
-    if isinstance(logits, tuple):
-        # Depending on the model and config, logits may contain extra tensors,
-        # like past_key_values, but logits always come first
-        logits = logits[0]
-    return logits.argmax(dim=-1)
+    return results
 
 train_dataset = dataset['train']
 train_dataset = train_dataset.map(lambda item: {'text': template(item, eval=False)})
